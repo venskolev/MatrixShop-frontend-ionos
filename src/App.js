48274894fa703ac758b70e7ca8425bd97800102a
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2022-06-20 15:36:30 +0200

    logo und favicon eingebaut

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <>
-    </>
+    <div className='App'>
+      <div className='bindLogo_box'>
+        <img className='bindLogo' src="./bindLogo_transparent.png" alt='BIND Logo' />
+      </div>
+    </div>
   );
 }
 
