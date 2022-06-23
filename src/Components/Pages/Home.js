54f3b37136ef7dc80370f8f0bdeae9a097c6eca2
Vens <v.kolev@alfatrex.com>
2022-06-23 15:29:03 +0200

    Carousel margin-top fix 3rem

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -2,13 +2,14 @@ import React from "react";
 import Carousels from "../Carousel/Carousel";
 import Footer from "../Footer/Footer";
 import Category from "./Category";
+import "../../sass/Pages.scss"
 
 
 
 export default function Home() {
   return (
     <>
-    <div className="home" style={{marginTop: "150px"}}>
+    <div className="home">
       
      <div>
      <Carousels />
