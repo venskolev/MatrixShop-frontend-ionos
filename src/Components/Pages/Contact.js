7b83acbf93429945e6888cbeecc17ed71b18bec1
Vens <v.kolev@alfatrex.com>
2022-06-23 15:04:30 +0200

    ContactForm erstellt frontend

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -1,11 +1,14 @@
 import React from "react";
+import ContactForm from "../Contact/Contact";
 import Footer from "../Footer/Footer";
+import "../../sass/Pages.scss"
 
 export default function Contact() {
   return (
     <>
     <div className="contact">
       <h1>Contact</h1>
+      <ContactForm />
     </div>
     <Footer />
     </>
