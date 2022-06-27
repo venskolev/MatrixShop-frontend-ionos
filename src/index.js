31755c995af167c50afc13cfdb41cd5e139de835
Vens <v.kolev@alfatrex.com>
2022-06-27 19:19:39 +0200

    Login und Register mit axios erstellt, backend noch nicht verbunden!

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { UserContextProvider } from "./Context/UserContext";
 
 
 import App from "./App";
@@ -7,5 +8,7 @@ import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<React.StrictMode>
+  <UserContextProvider>
   <App />
+  </UserContextProvider>
 </React.StrictMode>);
