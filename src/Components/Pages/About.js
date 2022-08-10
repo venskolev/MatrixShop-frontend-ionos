b1dd1c0e0bc6176f8df4a40547c0af25987e51db
Orhan Kadirov <89069508+OrhanKadirov@users.noreply.github.com>
2022-08-10 10:23:31 +0200

    über orhan beschreibung (#151)

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -208,31 +208,21 @@ Das Programmieren ist meine große Leidenschaft. Ich führe seit 2017 einen erfo
             </CardActions>
             <Collapse in={expanded2} timeout="auto" unmountOnExit>
               <CardContent>
-                <Typography paragraph>Method:</Typography>
                 <Typography paragraph>
-                  Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                  aside for 10 minutes.
+                Ich comme aus Bulgarien, seit 2012 lebe in Berlin.
                 </Typography>
                 <Typography paragraph>
-                  Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                  medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-                  occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                  large plate and set aside, leaving chicken and chorizo in the pan. Add
-                  pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-                  stirring often until thickened and fragrant, about 10 minutes. Add
-                  saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                Ich bin derzeit Student bei DCI, WebDev<br />
                 </Typography>
                 <Typography paragraph>
-                  Add rice and stir very gently to distribute. Top with artichokes and
-                  peppers, and cook without stirring, until most of the liquid is absorbed,
-                  15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-                  mussels, tucking them down into the rice, and cook again without
-                  stirring, until mussels have opened and rice is just tender, 5 to 7
-                  minutes more. (Discard any mussels that don&apos;t open.)
+                Ich habe gestartet zu lernen web development als hoby in 2016.
                 </Typography>
-                <Typography>
-                  Set aside off of the heat to let rest for 10 minutes, and then serve.
+                <Typography paragraph>
+                Damals habe ich mit wordpress angefangen dan 2018 mit python/django.
                 </Typography>
+                {/* <Typography>
+                  
+                </Typography> */}
               </CardContent>
             </Collapse>
           </Card>
